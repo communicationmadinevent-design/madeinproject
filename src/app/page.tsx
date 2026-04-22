c0228e9d31126628f99d51d63c2f8fbd489f8ee8
Tom EGE <communication.madinevent@gmail.com>
2026-04-22 15:17:03 +0200

    utiliser vercel.json pour servir HTML à la racine

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default function Home() {
-  const filePath = path.join(process.cwd(), "public", "madeinproject.html");
-  const htmlContent = fs.readFileSync(filePath, "utf-8");
-  
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
-}
